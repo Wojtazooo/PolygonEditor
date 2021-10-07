b79143a3987897bb181ee752a958670ed510af1b
Wojtek <wojtekulaski@gmail.com>
2021-10-07 18:05:30 +0200

    added form content

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace PolygonEditor
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,62 @@ namespace PolygonEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.formSplitter = new System.Windows.Forms.SplitContainer();
+            this.DrawingArea = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.formSplitter)).BeginInit();
+            this.formSplitter.Panel1.SuspendLayout();
+            this.formSplitter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // formSplitter
+            // 
+            this.formSplitter.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.formSplitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSplitter.Location = new System.Drawing.Point(0, 0);
+            this.formSplitter.Name = "formSplitter";
+            // 
+            // formSplitter.PanelDrawing
+            // 
+            this.formSplitter.Panel1.Controls.Add(this.DrawingArea);
+            this.formSplitter.Panel1MinSize = 800;
+            // 
+            // formSplitter.PanelTools
+            // 
+            this.formSplitter.Panel2MinSize = 300;
+            this.formSplitter.Size = new System.Drawing.Size(1184, 761);
+            this.formSplitter.SplitterDistance = 800;
+            this.formSplitter.TabIndex = 0;
+            // 
+            // DrawingArea
+            // 
+            this.DrawingArea.Location = new System.Drawing.Point(12, 12);
+            this.DrawingArea.Name = "DrawingArea";
+            this.DrawingArea.Size = new System.Drawing.Size(785, 737);
+            this.DrawingArea.TabIndex = 0;
+            this.DrawingArea.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.formSplitter);
+            this.MinimumSize = new System.Drawing.Size(1200, 800);
+            this.Name = "MainForm";
+            this.Text = "Polygon Editor";
+            this.formSplitter.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.formSplitter)).EndInit();
+            this.formSplitter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DrawingArea)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer formSplitter;
+        private System.Windows.Forms.PictureBox DrawingArea;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PolygonEditor
 {
-    public partial class Form1 : Form
+    public partial class MainForm : System.Windows.Forms.Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
