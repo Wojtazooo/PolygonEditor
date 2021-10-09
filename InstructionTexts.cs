0d26e55355e6846ca68a2e72db4ee6d37777ddeb
Wojtek <wojtekulaski@gmail.com>
2021-10-09 17:08:10 +0200

    instruction texts

---
+++ InstructionTexts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonEditor
+{
+    public static class InstructionTexts
+    {
+        public readonly static string[] AddPolygon = {
+            "Current option: Add Polygon",
+            "",
+            "Right Click: add new vertex",
+            "Left Click: finish",
+            "Escape: cancel"
+        };
+
+    }
+}
