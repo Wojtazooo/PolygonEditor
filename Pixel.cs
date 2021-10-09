a6c2918a18965fda4073ed581d5a7cf7878a664f
Wojtek <wojtekulaski@gmail.com>
2021-10-09 11:49:35 +0200

    extend Pixel

--- Pixel.cs
+++ Pixel.cs
@@ -9,15 +9,49 @@ namespace PolygonEditor
 {
     public class Pixel
     {
-        private Point _point;
+        public int X;
+        public int Y;
         public Color Color;
-        public int X => _point.X;
-        public int Y => _point.Y;
 
-        public Pixel(Point point, Color color)
+        public Pixel(int x, int y, Color color)
         {
-            this._point = point;
+            this.X = x;
+            this.Y = y;
             this.Color = color;
+        }
+
+        public Pixel(Point p, Color color)
+        {
+            this.X = p.X;
+            this.Y = p.Y;
+            this.Color = color;
+        }
+
+        public Pixel GetSymetricPixelByX(int x)
+        {
+            int symetricX = 2 * x - X;
+            return new Pixel(symetricX, Y, Color);
+        }
+
+        public Pixel GetSymetricPixelByY(int y)
+        {
+            int symetricY = 2 * y - Y;
+            return new Pixel(X, symetricY, Color);
+        }
+
+        public static List<Pixel> GetPixelsSymetricByX(List<Pixel> pixels, int x)
+        {
+            List<Pixel> xSymetricPixels = new List<Pixel>();
+            foreach (var p in pixels)
+                xSymetricPixels.Add(p.GetSymetricPixelByX(x));
+            return xSymetricPixels;
+        }
+        public static List<Pixel> GetPixelsSymetricByY(List<Pixel> pixels, int y)
+        {
+            List<Pixel> ySymetricPixels = new List<Pixel>();
+            foreach (var p in pixels)
+                ySymetricPixels.Add(p.GetSymetricPixelByY(y));
+            return ySymetricPixels;
         }
     }
 }
